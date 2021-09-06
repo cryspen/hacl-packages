cec402060caecfef182de6c58441421383f89a61
Franziskus Kiefer <franziskuskiefer@gmail.com>
2021-09-06 17:32:03 +0200

    wip

---
+++ make-dist.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+#
+# Generate other snapshots of the source code
+#
+#     The code snapshots are configured and can be built.
+#     However, we don't provide any helpers to do so.
+#     Algorithms and features that should be supported
+#     must be provided as input.   
+
+
+from argparse import ArgumentParser
+
+# The main parser to attach to with the decorator.
+cli = ArgumentParser()
+subparsers = cli.add_subparsers(dest="subcommand")
+
+# Decorator for sub commands.
+def subcommand(args=[], parent=subparsers):
+    def decorator(func):
+        parser = parent.add_parser(func.__name__, description=func.__doc__)
+        for arg in args:
+            parser.add_argument(*arg[0], **arg[1])
+        parser.set_defaults(func=func)
+    return decorator
+
+# Helper for subcommand decorator
+def argument(*name_or_flags, **kwargs):
+    return ([*name_or_flags], kwargs)
+
+# Sub command for building a snapshot with a subset of algorithms
+#
+# The config should be read from config.json.
+@subcommand([argument("-a", "--algorithms", help="The list of algorithms to include.", type=str),
+             argument("-f", "--features", help="The list of hardware features to include.", type=str),
+             argument("-b", "--hacl", help="Only enable hacl* without evercrypt.", action="store_true")])
+def snapshot(args):
+    # Parsing arguments to decide what to put into the snapshot.
+    if args.algorithms:
+        algorithms = [s.strip().lower() for s in args.algorithms.split(",")]
+    else:
+        algorithms = []
+    print("Enabling algorithms", algorithms)
+    if args.features:
+        features = [s.strip().lower() for s in args.features.split(",")]
+    else:
+        features = []
+    print("Enabling features", features)
+    hacl = args.hacl
+    if hacl:
+        print("Enabling only hacl* without evercrypt")
+    
+    # The HACL source code files
+    hacl_sources = {
+        "aes": ["Hacl_AES.c"],
+        "blake2b": ["Hacl_Blake2b_32.c", "Hacl_Blake2b_256.c"],
+        "chacha20poly1305": ["Hacl_Chacha20Poly1305_32.c", "Hacl_Chacha20Poly1305_128.c", "Hacl_Chacha20Poly1305_256.c"],
+    }
+    # Features required by each file.
+    hacl_features = {
+        "Hacl_AES.c": ["aesni"],
+        "Hacl_Blake2b_32.c": [],
+        "Hacl_Blake2b_256.c": ["avx2"],
+        "Hacl_Chacha20Poly1305_32.c": [],
+        "Hacl_Chacha20Poly1305_128.c": ["avx"],
+        "Hacl_Chacha20Poly1305_256.c": ["avx2"],
+        
+    }
+
+    # Get the sources and dependencies required by the requested
+    # algorithms and features.
+    enabled_sources = []
+    for a in algorithms:
+        if a in hacl_sources:
+            # Check if the required features were requested as well.
+            for hacl_file in hacl_sources[a]:
+                required_features = hacl_features[hacl_file]
+                available = True
+                for required_feature in required_features:
+                    if not required_feature in features:
+                        available = False
+                        break
+                if available:
+                    enabled_sources.append(hacl_file)
+        else:
+            print("\nAborting")
+            print("  Unknown algorithm '%s'\n" % a)
+            exit(1)
+
+    print("sources: ", enabled_sources)
+    
+
+# Boiler plate
+def main():
+    args = cli.parse_args()
+    if args.subcommand is None:
+        cli.print_help()
+    else:
+        args.func(args)
+
+if __name__ == '__main__':
+    main()
+
