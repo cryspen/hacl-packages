f927489261fa087167e172825d7e4c1b1831f0f8
Franziskus Kiefer <franziskuskiefer@gmail.com>
2021-10-11 19:45:52 +0200

    clang-format tests

--- tests/blake2s.cc
+++ tests/blake2s.cc
@@ -4,36 +4,47 @@
 #include "Hacl_Hash_Blake2s_128.h"
 #endif
 
-#include "util.h"
 #include "blake2_vectors.h"
+#include "util.h"
 
-bool print_test2s(int in_len, uint8_t *in, int key_len, uint8_t *key, int exp_len, uint8_t *exp)
+bool
+print_test2s(int in_len,
+             uint8_t* in,
+             int key_len,
+             uint8_t* key,
+             int exp_len,
+             uint8_t* exp)
 {
-    uint8_t comp[exp_len];
-    memset(comp, 0, exp_len * sizeof comp[0]);
+  uint8_t comp[exp_len];
+  memset(comp, 0, exp_len * sizeof comp[0]);
 
 #ifdef HACL_CAN_COMPILE_VEC128
-    Hacl_Blake2s_128_blake2s(exp_len, comp, in_len, in, key_len, key);
-    printf("testing blake2s vec-128:\n");
-    bool ok = print_result(exp_len, comp, exp);
+  Hacl_Blake2s_128_blake2s(exp_len, comp, in_len, in, key_len, key);
+  printf("testing blake2s vec-128:\n");
+  bool ok = print_result(exp_len, comp, exp);
 #else
-    printf(" !!! NO TESTS RUN! NO VEC128 SUPPORT! !!!\n");
-    bool ok = true;
+  printf(" !!! NO TESTS RUN! NO VEC128 SUPPORT! !!!\n");
+  bool ok = true;
 #endif
 
-    return ok;
+  return ok;
 }
 
 class Blake2sTesting : public ::testing::TestWithParam<blake2_test_vector>
-{
-};
+{};
 
 TEST_P(Blake2sTesting, TryTestVectors)
 {
-    const blake2_test_vector &vectors2s(GetParam());
-    bool test = print_test2s(vectors2s.input_len, vectors2s.input, vectors2s.key_len, vectors2s.key, vectors2s.expected_len, vectors2s.expected);
-    EXPECT_TRUE(test);
+  const blake2_test_vector& vectors2s(GetParam());
+  bool test = print_test2s(vectors2s.input_len,
+                           vectors2s.input,
+                           vectors2s.key_len,
+                           vectors2s.key,
+                           vectors2s.expected_len,
+                           vectors2s.expected);
+  EXPECT_TRUE(test);
 }
 
-INSTANTIATE_TEST_SUITE_P(TestVectors, Blake2sTesting,
+INSTANTIATE_TEST_SUITE_P(TestVectors,
+                         Blake2sTesting,
                          ::testing::ValuesIn(vectors2s));
--- tests/chacha20poly1305.cc
+++ tests/chacha20poly1305.cc
@@ -13,52 +13,105 @@
 #include "chacha20poly1305_vectors.h"
 
 // Function pointer to multiplex between the different implementations.
-typedef void (*test_encrypt)(uint8_t *, uint8_t *, uint32_t, uint8_t *, uint32_t, uint8_t *, uint8_t *, uint8_t *);
-typedef uint32_t (*test_decrypt)(uint8_t *, uint8_t *, uint32_t, uint8_t *, uint32_t, uint8_t *, uint8_t *, uint8_t *);
+typedef void (*test_encrypt)(uint8_t*,
+                             uint8_t*,
+                             uint32_t,
+                             uint8_t*,
+                             uint32_t,
+                             uint8_t*,
+                             uint8_t*,
+                             uint8_t*);
+typedef uint32_t (*test_decrypt)(uint8_t*,
+                                 uint8_t*,
+                                 uint32_t,
+                                 uint8_t*,
+                                 uint32_t,
+                                 uint8_t*,
+                                 uint8_t*,
+                                 uint8_t*);
 
-bool print_test(test_encrypt aead_encrypt, test_decrypt aead_decrypt, int in_len, uint8_t *in, uint8_t *key, uint8_t *nonce, int aad_len, uint8_t *aad, uint8_t *exp_mac, uint8_t *exp_cipher)
+bool
+print_test(test_encrypt aead_encrypt,
+           test_decrypt aead_decrypt,
+           int in_len,
+           uint8_t* in,
+           uint8_t* key,
+           uint8_t* nonce,
+           int aad_len,
+           uint8_t* aad,
+           uint8_t* exp_mac,
+           uint8_t* exp_cipher)
 {
-    uint8_t plaintext[in_len];
-    memset(plaintext, 0, in_len * sizeof plaintext[0]);
-    uint8_t ciphertext[in_len];
-    memset(ciphertext, 0, in_len * sizeof ciphertext[0]);
-    uint8_t mac[16] = {0};
+  uint8_t plaintext[in_len];
+  memset(plaintext, 0, in_len * sizeof plaintext[0]);
+  uint8_t ciphertext[in_len];
+  memset(ciphertext, 0, in_len * sizeof ciphertext[0]);
+  uint8_t mac[16] = { 0 };
 
-    (*aead_encrypt)(key, nonce, aad_len, aad, in_len, in, ciphertext, mac);
-    printf("Chacha20Poly1305 Result (chacha20):\n");
-    bool ok = print_result(in_len, ciphertext, exp_cipher);
-    printf("(poly1305):\n");
-    ok = ok && print_result(16, mac, exp_mac);
+  (*aead_encrypt)(key, nonce, aad_len, aad, in_len, in, ciphertext, mac);
+  printf("Chacha20Poly1305 Result (chacha20):\n");
+  bool ok = print_result(in_len, ciphertext, exp_cipher);
+  printf("(poly1305):\n");
+  ok = ok && print_result(16, mac, exp_mac);
 
-    int res = (*aead_decrypt)(key, nonce, aad_len, aad, in_len, plaintext, exp_cipher, exp_mac);
-    if (res != 0)
-        printf("AEAD Decrypt (Chacha20/Poly1305) failed \n.");
-    ok = ok && (res == 0);
-    ok = ok && print_result(in_len, plaintext, in);
+  int res = (*aead_decrypt)(
+    key, nonce, aad_len, aad, in_len, plaintext, exp_cipher, exp_mac);
+  if (res != 0)
+    printf("AEAD Decrypt (Chacha20/Poly1305) failed \n.");
+  ok = ok && (res == 0);
+  ok = ok && print_result(in_len, plaintext, in);
 
-    return ok;
+  return ok;
 }
 
-class Chacha20Poly1305Testing : public ::testing::TestWithParam<chacha20poly1305_test_vector>
-{
-};
+class Chacha20Poly1305Testing
+  : public ::testing::TestWithParam<chacha20poly1305_test_vector>
+{};
 
 TEST_P(Chacha20Poly1305Testing, TryTestVectors)
 {
-    const chacha20poly1305_test_vector &vectors(GetParam());
-    bool test = print_test(&Hacl_Chacha20Poly1305_32_aead_encrypt, &Hacl_Chacha20Poly1305_32_aead_decrypt, vectors.input_len, vectors.input, &vectors.key[0], &vectors.nonce[0], vectors.aad_len, vectors.aad, &vectors.tag[0], vectors.cipher);
-    EXPECT_TRUE(test);
+  const chacha20poly1305_test_vector& vectors(GetParam());
+  bool test = print_test(&Hacl_Chacha20Poly1305_32_aead_encrypt,
+                         &Hacl_Chacha20Poly1305_32_aead_decrypt,
+                         vectors.input_len,
+                         vectors.input,
+                         &vectors.key[0],
+                         &vectors.nonce[0],
+                         vectors.aad_len,
+                         vectors.aad,
+                         &vectors.tag[0],
+                         vectors.cipher);
+  EXPECT_TRUE(test);
 
 #ifdef HACL_CAN_COMPILE_VEC128
-    test = print_test(&Hacl_Chacha20Poly1305_128_aead_encrypt, &Hacl_Chacha20Poly1305_128_aead_decrypt, vectors.input_len, vectors.input, &vectors.key[0], &vectors.nonce[0], vectors.aad_len, vectors.aad, &vectors.tag[0], vectors.cipher);
-    EXPECT_TRUE(test);
+  test = print_test(&Hacl_Chacha20Poly1305_128_aead_encrypt,
+                    &Hacl_Chacha20Poly1305_128_aead_decrypt,
+                    vectors.input_len,
+                    vectors.input,
+                    &vectors.key[0],
+                    &vectors.nonce[0],
+                    vectors.aad_len,
+                    vectors.aad,
+                    &vectors.tag[0],
+                    vectors.cipher);
+  EXPECT_TRUE(test);
 #endif // HACL_CAN_COMPILE_VEC128
 
 #ifdef HACL_CAN_COMPILE_VEC256
-    test = print_test(&Hacl_Chacha20Poly1305_256_aead_encrypt, &Hacl_Chacha20Poly1305_256_aead_decrypt, vectors.input_len, vectors.input, &vectors.key[0], &vectors.nonce[0], vectors.aad_len, vectors.aad, &vectors.tag[0], vectors.cipher);
-    EXPECT_TRUE(test);
+  test = print_test(&Hacl_Chacha20Poly1305_256_aead_encrypt,
+                    &Hacl_Chacha20Poly1305_256_aead_decrypt,
+                    vectors.input_len,
+                    vectors.input,
+                    &vectors.key[0],
+                    &vectors.nonce[0],
+                    vectors.aad_len,
+                    vectors.aad,
+                    &vectors.tag[0],
+                    vectors.cipher);
+  EXPECT_TRUE(test);
 #endif // HACL_CAN_COMPILE_VEC256
 }
 
-INSTANTIATE_TEST_SUITE_P(TestVectors, Chacha20Poly1305Testing,
+INSTANTIATE_TEST_SUITE_P(TestVectors,
+                         Chacha20Poly1305Testing,
                          ::testing::ValuesIn(vectors));
