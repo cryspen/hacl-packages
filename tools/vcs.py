f7851963ef02e227ffd4a617f91d8d13580d2971
Franziskus Kiefer <franziskuskiefer@gmail.com>
2021-10-15 14:50:38 +0200

    c89 and msvc update

--- tools/vcs.py
+++ tools/vcs.py
@@ -35,32 +35,45 @@ def update(args):
 
     # get absolute src and include directories
     my_dir = os.path.abspath(join(os.path.dirname(__file__), '..'))
-    src_dir = join(my_dir, 'src')
-    include_dir = join(my_dir, 'include')
 
     # clone hacl into a temp directory
     temp_hacl_dir = mkdtemp()
     subprocess.run(['git', 'clone', '--depth', '1',
                    upstream_url, temp_hacl_dir], check=True)
+
+    def copy_files(new_dist, src_dir, include_dir):
+        # remove all .c and .h files from the src/include directory
+        for f in all_files(src_dir, '.c'):
+            os.remove(f)
+        for f in all_files(include_dir, '.h'):
+            os.remove(f)
+
+        # copy new .c and .h files to the src/include directory
+        def only_c(d, files):
+            files = [f for f in files if isfile(
+                join(d, f)) and f[-2:] != '.c' or "vale" in f.lower()]
+            return files
+        copytree(new_dist, src_dir, ignore=only_c, dirs_exist_ok=True)
+
+        def only_h(d, files):
+            files = [f for f in files if isfile(
+                join(d, f)) and f[-2:] != '.h' or "vale" in f.lower()]
+            return files
+        copytree(new_dist, include_dir, ignore=only_h, dirs_exist_ok=True)
+
+    src_dir = join(my_dir, 'src')
+    include_dir = join(my_dir, 'include')
     new_dist = os.path.join(temp_hacl_dir, 'dist', 'gcc-compatible')
+    copy_files(new_dist, src_dir, include_dir)
 
-    # remove all .c and .h files from the src/include directory
-    for f in all_files(src_dir, '.c'):
-        os.remove(f)
-    for f in all_files(include_dir, '.h'):
-        os.remove(f)
+    src_dir = join(src_dir, 'c89')
+    include_dir = join(include_dir, 'c89')
+    c89_dist = os.path.join(temp_hacl_dir, 'dist', 'c89-compatible')
+    copy_files(c89_dist, src_dir, include_dir)
 
-    # copy new .c and .h files to the src/include directory
-    def only_c(d, files):
-        files = [f for f in files if isfile(
-            join(d, f)) and f[-2:] != '.c' or "vale" in f.lower()]
-        return files
-    copytree(new_dist, src_dir, ignore=only_c, dirs_exist_ok=True)
-
-    def only_h(d, files):
-        files = [f for f in files if isfile(
-            join(d, f)) and f[-2:] != '.h' or "vale" in f.lower()]
-        return files
-    copytree(new_dist, include_dir, ignore=only_h, dirs_exist_ok=True)
+    src_dir = join(my_dir, 'src', 'msvc')
+    include_dir = join(my_dir, 'include', 'msvc')
+    msvc_dist = os.path.join(temp_hacl_dir, 'dist', 'msvc-compatible')
+    copy_files(msvc_dist, src_dir, include_dir)
 
     print(" [mach] 💪  Copied all new .c and .h files from hacl-star.")
