a11de34e10c409dc9c5ae3a6e029752ab14ee8aa
Franziskus Kiefer <franziskuskiefer@gmail.com>
2021-11-23 16:17:35 +0100

    x25519 wycheproof

--- tests/x25519.cc
+++ tests/x25519.cc
@@ -1,4 +1,7 @@
+#include <fstream>
+
 #include <gtest/gtest.h>
+#include <nlohmann/json.hpp>
 
 #include "Hacl_Curve25519_51.h"
 #include "curve25519_vectors.h"
@@ -11,6 +14,10 @@
 
 #include "config.h"
 #include "util.h"
+
+using json = nlohmann::json;
+
+#define bytes std::vector<uint8_t>
 
 TEST(x25519Test, HaclTest)
 {
@@ -26,3 +33,72 @@ TEST(x25519Test, HaclTest)
 #endif
   }
 }
+
+//=== Wycheproof tests ====
+
+typedef struct
+{
+  bytes public_key;
+  bytes private_key;
+  bytes shared;
+  bool valid;
+} TestCase;
+
+std::vector<TestCase>
+read_json()
+{
+
+  // Read JSON test vector
+  std::string test_dir = TEST_DIR;
+  test_dir += "/x25519_test.json";
+  std::ifstream json_test_file(test_dir);
+  json test_vectors;
+  json_test_file >> test_vectors;
+
+  std::vector<TestCase> tests_out;
+
+  // Read test group
+  for (auto& test : test_vectors["testGroups"].items()) {
+    auto test_value = test.value();
+
+    auto tests = test_value["tests"];
+    for (auto& test_case : tests.items()) {
+      auto test_case_value = test_case.value();
+      auto private_key = from_hex(test_case_value["private"]);
+      auto public_key = from_hex(test_case_value["public"]);
+      auto shared = from_hex(test_case_value["shared"]);
+      auto result = test_case_value["result"];
+      bool valid = result == "valid" || result == "acceptable";
+
+      tests_out.push_back({ public_key, private_key, shared, valid });
+    }
+  }
+
+  return tests_out;
+}
+
+class X25519Wycheproof : public ::testing::TestWithParam<TestCase>
+{};
+
+TEST_P(X25519Wycheproof, TryWycheproof)
+{
+  const TestCase& test_case(GetParam());
+
+  // Stupid const
+  uint8_t* private_key = const_cast<uint8_t*>(test_case.private_key.data());
+  uint8_t* public_key = const_cast<uint8_t*>(test_case.public_key.data());
+
+  uint8_t computed_shared[32] = { 0 };
+  Hacl_Curve25519_51_ecdh(computed_shared, private_key, public_key);
+  if (test_case.valid) {
+    EXPECT_EQ(std::vector<uint8_t>(computed_shared, computed_shared + 32),
+              test_case.shared);
+  } else {
+    EXPECT_NE(std::vector<uint8_t>(computed_shared, computed_shared + 32),
+              test_case.shared);
+  }
+}
+
+INSTANTIATE_TEST_SUITE_P(Wycheproof,
+                         X25519Wycheproof,
+                         ::testing::ValuesIn(read_json()));
