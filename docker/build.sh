d3668efa0a187b9ce1dd168c8b27941a410dd5bd
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2022-01-04 21:45:55 -0800

    fix things

--- docker/build.sh
+++ docker/build.sh
@@ -4,13 +4,13 @@ set -e
 set -o pipefail
 
 if [[ $1 == "-h" || $1 == "--help" || $1 == "" ]]; then
-  echo "Usage: $0 [HACL-REVISION]"
+  echo "Usage: $0 [EVEREST-REVISION]"
   echo
-  echo "Creates a container with a successful build of the provided HACL* revision"
+  echo "Creates a container with a successful build of HACL* based on the provided Everest revision"
   exit 1
 fi
 
 # Essentially this, with a few customizations:
 # https://raw.githubusercontent.com/project-everest/everest-ci/master/server-infra/linux/.docker/Dockerfile
 docker build -t everest_base_image:1 everest --progress=plain
-docker build . --progress=plain --build-arg hacl_revision=$1
+docker build . --progress=plain --build-arg everest_revision=$1
