1b633d4c707e4b5287805c43153a9eec03381306
Franziskus Kiefer <franziskuskiefer@gmail.com>
2022-02-16 19:43:49 +0000

    started s390x cross build

---
+++ config/s390x.cmake
@@ -0,0 +1,10 @@
+# Toolchain file compiling for s390x
+
+set(triple s390x-linux-gnu)
+set(arch s390x)
+set(HACL_TARGET_OS linux)
+set(CMAKE_C_COMPILER_TARGET ${triple})
+set(CMAKE_CXX_COMPILER_TARGET ${triple})
+# This isn't working unfortunately. It's being set in CMakeLists.txt again
+set(CMAKE_SYSTEM_PROCESSOR s390x)
+# message(FATAL_ERROR "FOOO: ${CMAKE_SYSTEM_PROCESSOR}")
