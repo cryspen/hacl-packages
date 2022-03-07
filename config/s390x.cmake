08da02dad695e234e9c92d9ca3c3a87cfac6ba81
Franziskus Kiefer <franziskuskiefer@gmail.com>
2022-03-07 11:27:53 +0100

    set s390x system name

--- config/s390x.cmake
+++ config/s390x.cmake
@@ -3,6 +3,7 @@
 set(triple s390x-linux-gnu)
 set(arch s390x)
 set(HACL_TARGET_OS linux)
+set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_C_COMPILER_TARGET ${triple})
 set(CMAKE_CXX_COMPILER_TARGET ${triple})
 # This isn't working unfortunately. It's being set in CMakeLists.txt again
