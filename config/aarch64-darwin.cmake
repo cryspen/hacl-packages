a2aa56c341bbe2d56e3d22418dc0f69fd9cc30dd
Franziskus Kiefer <franziskuskiefer@gmail.com>
2022-03-07 11:22:58 +0100

    maybe fixed cmake issues

--- config/aarch64-darwin.cmake
+++ config/aarch64-darwin.cmake
@@ -2,11 +2,16 @@
 
 set(triple arm64-apple-macos12)
 set(arch arm64)
-set(HACL_TARGET_OS osx)
+
+# For some reason we have to set the system name here in order to make the
+# CMAKE_SYSTEM_PROCESSOR being picked up correctly.
+set(CMAKE_SYSTEM_NAME Darwin)
+set(CMAKE_SYSTEM_VERSION "${CMAKE_HOST_SYSTEM_VERSION}")
+set(CMAKE_SYSTEM_PROCESSOR ${arch})
 set(CMAKE_C_COMPILER clang)
 set(CMAKE_C_COMPILER_TARGET ${triple})
 set(CMAKE_CXX_COMPILER clang++)
 set(CMAKE_CXX_COMPILER_TARGET ${triple})
-# This isn't working unfortunately. It's being set in CMakeLists.txt again
-set(CMAKE_SYSTEM_PROCESSOR ${arch})
 set(CMAKE_OSX_ARCHITECTURES ${arch})
+
+set(HACL_TARGET_OS osx)
