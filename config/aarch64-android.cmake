cbd9739d192c07b74b4e5f337d837c563daa1f4f
Franziskus Kiefer <franziskuskiefer@gmail.com>
2022-03-07 15:21:50 +0100

    make ndk path configurable

--- config/aarch64-android.cmake
+++ config/aarch64-android.cmake
@@ -2,14 +2,13 @@
 
 set(triple aarch64-linux-android)
 set(arch aarch64)
+
+# For some reason we have to set the system name here in order to make the
+# CMAKE_SYSTEM_PROCESSOR being picked up correctly.
 set(CMAKE_SYSTEM_NAME Android)
-set(CMAKE_ANDROID_ARCH_ABI arm64-v8a)
-set(CMAKE_ANDROID_NDK /Users/franziskus/Library/Android/sdk/ndk/23.1.7779620)
-set(HACL_TARGET_OS android)
-# set(CMAKE_C_COMPILER clang)
-# set(CMAKE_C_COMPILER_TARGET ${triple})
-# set(CMAKE_CXX_COMPILER clang++)
-# set(CMAKE_CXX_COMPILER_TARGET ${triple})
-# This isn't working unfortunately. It's being set in CMakeLists.txt again
+# We don't set the cmake version here as Android comes with an ancient cmake.
+# set(CMAKE_SYSTEM_VERSION "${CMAKE_HOST_SYSTEM_VERSION}")
 set(CMAKE_SYSTEM_PROCESSOR ${arch})
-set(CMAKE_OSX_ARCHITECTURES ${arch})
+set(CMAKE_ANDROID_ARCH_ABI arm64-v8a)
+set(CMAKE_ANDROID_NDK "${ANDROID_NDK_PATH}")
+set(HACL_TARGET_OS android)
