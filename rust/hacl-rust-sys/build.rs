192b5cd7c95796bc94ae2cfde6c12caba35c7745
Franziskus Kiefer <franziskuskiefer@gmail.com>
2022-03-29 12:16:44 +0200

    allow standalone rust build

--- rust/hacl-rust-sys/build.rs
+++ rust/hacl-rust-sys/build.rs
@@ -1,7 +1,7 @@
 #[cfg(not(windows))]
 extern crate bindgen;
 
-use std::{env, path::Path};
+use std::{env, path::Path, process::Command};
 
 #[cfg(not(windows))]
 fn create_bindings(include_path: &Path, home_dir: &Path) {
@@ -51,27 +51,91 @@ fn create_bindings(include_path: &Path, home_dir: &Path) {
 #[cfg(windows)]
 fn create_bindings(_: &Path, _: &Path) {}
 
+fn get_hacl_c(out_path: &Path) {
+    // git clone the repo
+    if out_path.join("hacl-packages").exists() {
+        // Only clone if we didn't do so already.
+        return;
+    }
+    let mut mach_cmd = Command::new("git");
+    let mach_status = mach_cmd
+        .current_dir(out_path)
+        .args(&[
+            "clone",
+            "-b",
+            "franziskus/rust-rework",
+            "https://github.com/cryspen/hacl-packages",
+            "--depth=1",
+        ])
+        .status()
+        .expect("Failed to run git clone.");
+    if !mach_status.success() {
+        panic!("Failed to run git clone.")
+    }
+}
+
+fn build_hacl_c(path: &Path) {
+    println!(" >>> Building HACL C in {}", path.display());
+    let mut mach_cmd = Command::new("./mach");
+    let mach_status = mach_cmd
+        .current_dir(path)
+        // We always build the release version here.
+        // For debugging don't use this.
+        .args(&["build", "--release"])
+        .status()
+        .expect("Failed to run mach build.");
+    if !mach_status.success() {
+        panic!("Failed to run mach build.")
+    }
+    let install_path = path.join("build").join("installed");
+    println!(" >>> Installing HACL C into {}", install_path.display());
+    let mut mach_cmd = Command::new("./mach");
+    let mach_status = mach_cmd
+        .current_dir(path)
+        .args(&[
+            "install",
+            "--prefix",
+            install_path.to_str().unwrap(),
+            "-c",
+            "release",
+        ])
+        .status()
+        .expect("Failed to run mach install.");
+    if !mach_status.success() {
+        panic!("Failed to run mach install.")
+    }
+}
+
 fn main() {
     // Get ENV variables
     let home_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let home_dir = Path::new(&home_dir);
     let target = env::var("TARGET").unwrap();
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let out_path = Path::new(&out_dir);
+    let mach_build = env::var("MACH_BUILD").ok().is_some();
 
-    // Set library name to look up
-    let library_name = if target == "x86_64-pc-windows-msvc" {
-        "libhacl_static"
+    // Get the C library and build it first.
+    // This is the default behaviour. It can be disabled when working on this
+    // to pick up the local version. This is what the global mach script does.
+    let hacl_path = if !mach_build {
+        get_hacl_c(&out_path);
+        let hacl_packages_path = out_path.join("hacl-packages");
+        build_hacl_c(&hacl_packages_path);
+        hacl_packages_path.join("build").join("installed")
     } else {
-        "hacl_static"
+        // Use the higher level install directory.
+        home_dir
+            .join("..")
+            .join("..")
+            .join("build")
+            .join("installed")
     };
-
-    // Set HACL paths
-    let hacl_path = home_dir
-        .join("..")
-        .join("..")
-        .join("build")
-        .join("installed");
     let hacl_lib_path = hacl_path.join("lib");
     let hacl_include_path = hacl_path.join("include");
+
+    // Set library name to look up
+    let library_name = "hacl_static";
 
     // Set re-run trigger
     println!("cargo:rerun-if-changed=wrapper.h");
