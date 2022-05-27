5e60a00a7bfec3642560fbfa5cd29c6556304e18
Franziskus Kiefer <franziskuskiefer@gmail.com>
2022-05-27 10:52:09 +0200

    make it work - config.h needs to be in path. It is in build/ - The Makefile needs to know the platform/toolchain capabilities in order to blocklist unsupported files. This is generated by cmake. I had to adapt it a little to make it work in this setup.

--- opam.sh
+++ opam.sh
@@ -21,3 +21,11 @@ cp ocaml/Makefile opam/hacl-star-raw
 cp ocaml/ctypes.depend opam/hacl-star-raw
 cp ocaml/META opam/hacl-star-raw
 cp ocaml/hacl-star-raw.opam opam
+
+cp -r ocaml/hacl-star opam/
+
+make -C opam/hacl-star-raw libhacl.dylib libhacl_static.a
+# We need to pick up the environment variables from the cmake build for opam
+source opam/hacl-star-raw/build/cached-config.txt
+make -C opam/hacl-star-raw -j
+make -C opam/hacl-star-raw -j test
