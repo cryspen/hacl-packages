174b0af83ceb070e3ea309282ed1bbd98ea01c1f
Victor Dumitrescu <v.dumitrescu@outlook.com>
2022-07-11 18:31:19 +0200

    opam.sh: don't copy unnecessary source files

--- opam.sh
+++ opam.sh
@@ -8,10 +8,13 @@ mkdir opam
 mkdir opam/hacl-star-raw
 cp -r config opam/hacl-star-raw
 mv opam/hacl-star-raw/config/default_config.cmake opam/hacl-star-raw/config/config.cmake
-cp -r src opam/hacl-star-raw
+mkdir opam/hacl-star-raw/src
+cp src/* opam/hacl-star-raw/src/ | true
+mkdir opam/hacl-star-raw/include
+cp include/* opam/hacl-star-raw/include/ | true
+cp -r include/internal opam/hacl-star-raw/include/
 cp -r vale opam/hacl-star-raw
 cp -r karamel opam/hacl-star-raw
-cp -r include opam/hacl-star-raw
 
 cp CMakeLists.txt opam/hacl-star-raw
 
