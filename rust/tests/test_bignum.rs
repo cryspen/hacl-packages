f46a7d6a928ab1e1ad20f1aef1e07fdb9f1b8138
Jeffrey Goldberg <jeff@agilebits.com>
2022-07-11 21:50:23 -0600

    Yay! No memory leak

--- rust/tests/test_bignum.rs
+++ rust/tests/test_bignum.rs
@@ -21,3 +21,43 @@ fn test_partial_ord() {
     assert!(!(v1337_bn > v2038_bn), "1337 should not be gt 2038");
     assert!(!(v1337_bn == v2038_bn), "1337 should not be equal to 2038");
 }
+
+use rand::prelude::SmallRng;
+use rand::{RngCore, SeedableRng};
+
+#[test]
+#[ignore]
+// Run
+//   cargo test test_memleak -- --ignored
+// which may take several minutes.
+// That should give you plenty of time to see if processes with names matching
+// /test_bignum-*/ grow in memory.
+fn test_memleak() {
+    let trials = 5_000_000_u64;
+    let mut small_rng = SmallRng::seed_from_u64(123_u64);
+
+    for _ in 0..trials {
+        let data = &mut vec![0_u8; 512];
+        data.fill(0);
+
+        small_rng.fill_bytes(&mut data[..]);
+        let a = Bignum::new(data.to_vec()).unwrap();
+
+        small_rng.fill_bytes(&mut data[..]);
+        let b = Bignum::new(data.to_vec()).unwrap();
+
+        let mut true_count = 0;
+
+        if a < b {
+            true_count += 1;
+        }
+        if a > b {
+            true_count += 1;
+        }
+        if a == b {
+            true_count += 1;
+        }
+
+        assert!(true_count == 1, "We have the wrong number of truths.")
+    }
+}
