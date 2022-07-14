25cdb17f5d9927435605d71f29dcdb26ba08bc14
Jeffrey Goldberg <jeff@agilebits.com>
2022-07-13 19:19:29 -0600

    Bug is with leading 0 bytes.

--- rust/tests/test_bignum.rs
+++ rust/tests/test_bignum.rs
@@ -1,13 +1,76 @@
+
 mod test_util;
-use hex;
-use test_util::*;
+
+use std::mem;
+use std::fmt;
+use data_encoding::HEXLOWER;
+
+
 
 use hacl_rust::bignum::{Bignum, Error};
+
+use rand::prelude::SmallRng;
+use rand::{RngCore, SeedableRng};
 
 fn bn_from_u64(n: u64) -> Bignum {
     let b = n.to_be_bytes();
     let bn: Result<Bignum, Error> = Bignum::new(&b);
     bn.unwrap()
+}
+
+#[test]
+fn test_to_from() {
+    let trials = 1_000;
+    let mut small_rng = SmallRng::seed_from_u64(123_u64);
+
+    #[derive(Clone)]
+    struct Failure {
+        a: Vec<u8>,
+        b: Vec<u8>,
+        trial: u32,
+    }
+    impl fmt::Debug for Failure {
+        fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+            fmt.debug_struct("Failure")
+                .field("a", &self.a)
+                .field("b", &self.b)
+                .field("trial", &self.trial)
+                .finish()
+        }
+    }
+
+    let mut failures: Vec<Failure> = Vec::new();
+
+    for trial in 0..trials {
+        // we create random a: u128 and a_bn: Bignum
+        // which should have same numeric value.
+        let mut dest: [u8; 512] = unsafe { mem::zeroed() };
+        small_rng.fill_bytes(&mut dest);
+
+        // Thank you slack exchange. I never would have figured that this
+        // is the way to make an immutable copy of a mutable object.
+        let a_data = dest;
+        let a_vec = dest.to_vec();
+
+        let a_hex = HEXLOWER.encode(&a_data);
+    
+        let a_bn = Bignum::new(&a_data).unwrap();
+        let b_vec = a_bn.to_vec8();
+
+        let out_hex = HEXLOWER.encode(&b_vec);
+
+        if a_hex != out_hex {
+            let f = &Failure{
+                a: a_vec.clone(),
+                b: b_vec.clone(),
+                trial,
+            };
+            failures.push(f.clone());
+        }
+
+        assert!(failures.is_empty(), "There were {} in-out failure(s) out of {} trials\n{:?}",
+            failures.len(), trials, failures);
+    }
 }
 
 #[test]
@@ -18,7 +81,6 @@ fn test_partial_ord() {
 
     let dest: &mut [u8; 16] = &mut [0; 16];
     for trial in 0..trials {
-
         // we create random a: u128 and a_bn: Bignum
         // which should have same numeric value.
         // And we do the same for b and b_bn.
@@ -27,20 +89,23 @@ fn test_partial_ord() {
         let a_bn = Bignum::new(dest).unwrap();
         small_rng.fill_bytes(dest);
         let b = u128::from_be_bytes(*dest);
-        let b_bn =  Bignum::new(dest).unwrap();
+        let b_bn = Bignum::new(dest).unwrap();
 
         assert!(a_bn == a_bn);
         let i_cmp = a.partial_cmp(&b).unwrap();
         let b_cmp = a_bn.partial_cmp(&b_bn).unwrap();
 
-        assert!(i_cmp == b_cmp,
+        assert!(
+            i_cmp == b_cmp,
             "i_cmp {:?} != b_cmp {:?} for (a={},b={}) in trial {}",
-            i_cmp, b_cmp, a, b, trial);
+            i_cmp,
+            b_cmp,
+            a,
+            b,
+            trial
+        );
     }
 }
-
-use rand::prelude::SmallRng;
-use rand::{RngCore, SeedableRng};
 
 #[test]
 #[ignore]
