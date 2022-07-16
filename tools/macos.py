e1bc05314e29cd6e4a3557ccb11a7be7a3ad7af7
Damian Poddebniak <poddebniak@mailbox.org>
2022-07-16 23:52:08 +0200

    Reformat code with ufmt (black + µsort)
    See https://github.com/omnilib/ufmt

--- tools/macos.py
+++ tools/macos.py
@@ -10,14 +10,16 @@ import subprocess
 
 
 def ios_sysroot():
-    '''Returns the sysroot of the iOS SDK'''
-    result = subprocess.run("xcrun --sdk iphoneos --show-sdk-path",
-                            stdout=subprocess.PIPE,
-                            shell=True,
-                            check=True)
-    return result.stdout.decode('utf-8')[:-1]
+    """Returns the sysroot of the iOS SDK"""
+    result = subprocess.run(
+        "xcrun --sdk iphoneos --show-sdk-path",
+        stdout=subprocess.PIPE,
+        shell=True,
+        check=True,
+    )
+    return result.stdout.decode("utf-8")[:-1]
 
 
 def aarch64_ios_args():
-    '''Returns clang arguments to build for aarch64 iOS'''
-    return ['-isysroot', ios_sysroot()]
+    """Returns clang arguments to build for aarch64 iOS"""
+    return ["-isysroot", ios_sysroot()]
