166da7e996f6520ec9c64f988fa7136440355c36
Damian Poddebniak <poddebniak@mailbox.org>
2022-08-02 10:59:30 +0200

    Add script to aggregate test coverage of all tests.

---
+++ tools/coverage.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+
+set -e
+
+echo "[!] Generating coverage report ... "
+pushd build/Debug
+
+mkdir -p coverage/full/html
+llvm-profdata merge -sparse *.profraw -o coverage/full/full.profdata
+llvm-cov export \
+	-format lcov \
+	--instr-profile coverage/full/full.profdata \
+	"blake2b" \
+	-object "blake2s" \
+	-object "chacha20poly1305" \
+	-object "ed25519" \
+	-object "hmac" \
+	-object "p256_ecdh" \
+	-object "p256_ecdsa" \
+	-object "p256k1_ecdsa" \
+	-object "rsapss" \
+	-object "sha2" \
+	-object "sha3" \
+	-object "x25519" \
+	../../src/*.c \
+	> coverage/full/full.lcov
+genhtml coverage/full/full.lcov -o coverage/full/html
+
+popd
+echo "[!] ... done"
