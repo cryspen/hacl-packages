b83a75e30aee0cbef35a03b40af437ae83f3a641
Damian Poddebniak <poddebniak@mailbox.org>
2022-08-03 12:26:59 +0200

    Make coverage.sh work under macOS.

--- tools/coverage.sh
+++ tools/coverage.sh
@@ -6,24 +6,20 @@ set -e
 echo "[!] Generating coverage report ... "
 pushd build/Debug
 
+hacl=""
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	hacl=libhacl.so
+else
+	hacl=libhacl.dylib
+fi
+
 mkdir -p coverage/full/html
 llvm-profdata merge -sparse *.profraw -o coverage/full/full.profdata
 llvm-cov export \
 	-format lcov \
 	--instr-profile coverage/full/full.profdata \
-	libhacl.so \
-	-object "blake2b" \
-	-object "blake2s" \
-	-object "chacha20poly1305" \
-	-object "ed25519" \
-	-object "hmac" \
-	-object "p256_ecdh" \
-	-object "p256_ecdsa" \
-	-object "p256k1_ecdsa" \
-	-object "rsapss" \
-	-object "sha2" \
-	-object "sha3" \
-	-object "x25519" \
+	$hacl \
 	../../src/ \
 	> coverage/full/full.lcov
 genhtml coverage/full/full.lcov -o coverage/full/html
