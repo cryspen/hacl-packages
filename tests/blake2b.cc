684e2f75f333d86dd1c27cdda57e99928b7f17f7
Damian Poddebniak <poddebniak@mailbox.org>
2022-08-09 10:48:13 +0200

    tests: Extend blake2{b,s} tests.

--- tests/blake2b.cc
+++ tests/blake2b.cc
@@ -13,7 +13,7 @@
 #include "hacl-cpu-features.h"
 
 #include "Hacl_Hash_Blake2.h"
-#include "blake2_vectors.h"
+#include "Hacl_Streaming_Blake2.h"
 #include "config.h"
 #include "util.h"
 
@@ -21,6 +21,7 @@
 
 #ifdef HACL_CAN_COMPILE_VEC256
 #include "Hacl_Hash_Blake2b_256.h"
+#include "Hacl_Streaming_Blake2b_256.h"
 #endif
 
 #define VALE                                                                   \
@@ -34,65 +35,6 @@
 
 using json = nlohmann::json;
 
-// Function pointer to multiplex between the different implementations.
-typedef void (
-  *test_blake)(uint32_t, uint8_t*, uint32_t, uint8_t*, uint32_t, uint8_t*);
-
-bool
-test_blake2b(test_blake blake,
-             size_t input_len,
-             uint8_t* input,
-             size_t key_len,
-             uint8_t* key,
-             size_t expected_len,
-             uint8_t* expected)
-{
-  bytes comp(expected_len, 0);
-  (*blake)(expected_len, comp.data(), input_len, input, key_len, key);
-  return compare_and_print(expected_len, comp.data(), expected);
-}
-
-class Blake2bTesting : public ::testing::TestWithParam<blake2_test_vector>
-{};
-
-TEST_P(Blake2bTesting, TryTestVectors)
-{
-  const blake2_test_vector& vectors2b(GetParam());
-  bool test = test_blake2b(&Hacl_Blake2b_32_blake2b,
-                           vectors2b.input_len,
-                           vectors2b.input,
-                           vectors2b.key_len,
-                           vectors2b.key,
-                           vectors2b.expected_len,
-                           vectors2b.expected);
-  EXPECT_TRUE(test);
-
-#ifdef HACL_CAN_COMPILE_VEC256
-  // We might have compiled vec256 blake2b but don't have it available on the
-  // CPU when running now.
-  if (hacl_vec256_support()) {
-    test = test_blake2b(&Hacl_Blake2b_256_blake2b,
-                        vectors2b.input_len,
-                        vectors2b.input,
-                        vectors2b.key_len,
-                        vectors2b.key,
-                        vectors2b.expected_len,
-                        vectors2b.expected);
-    EXPECT_TRUE(test);
-  } else {
-    printf(" ! Vec256 was compiled but AVX2 is not available on this CPU.\n");
-  }
-#endif
-}
-
-INSTANTIATE_TEST_SUITE_P(TestVectors,
-                         Blake2bTesting,
-                         ::testing::ValuesIn(vectors2b));
-
-// === Test vectors === //
-
-#define bytes std::vector<uint8_t>
-
 typedef struct
 {
   size_t out_len;
@@ -102,12 +44,9 @@ typedef struct
 } TestCase;
 
 std::vector<TestCase>
-read_json()
+read_blake2b_json(std::string path)
 {
-
-  // Read JSON test vector
-  std::string test_dir = "blake2b.json";
-  std::ifstream json_test_file(test_dir);
+  std::ifstream json_test_file(path);
   json test_vectors;
   json_test_file >> test_vectors;
 
@@ -128,12 +67,10 @@ read_json()
 }
 
 std::vector<TestCase>
-read_official_json()
+read_official_json(std::string path)
 {
-
   // Read JSON test vector
-  std::string test_dir = "official.json";
-  std::ifstream json_test_file(test_dir);
+  std::ifstream json_test_file(path);
   json test_vectors;
   json_test_file >> test_vectors;
 
@@ -150,48 +87,152 @@ read_official_json()
       auto key = from_hex(test_case["key"]);
 
       tests_out.push_back({ out_len, digest, input, key });
+    } else if (test_case["hash"] == "blake2bp") {
+      // Skipping
+    } else if (test_case["hash"] == "blake2xb") {
+      // Skipping
+    } else if (test_case["hash"] == "blake2s") {
+      // Skipping
+    } else if (test_case["hash"] == "blake2sp") {
+      // Skipping
+    } else if (test_case["hash"] == "blake2xs") {
+      // Skipping
+    } else {
+      throw "Unexpected hash value.";
     }
   }
 
   return tests_out;
 }
 
-class Blake2bKAT : public ::testing::TestWithParam<TestCase>
+class Blake2b : public ::testing::TestWithParam<TestCase>
 {};
 
-TEST_P(Blake2bKAT, TryKAT)
+TEST_P(Blake2b, KAT)
 {
-  // Initialize CPU feature detection
-  hacl_init_cpu_features();
-  EverCrypt_AutoConfig2_init();
-  const TestCase& test_case(GetParam());
+  auto test = GetParam();
 
-  // Stupid const
-  uint8_t* input = const_cast<uint8_t*>(test_case.input.data());
-  uint8_t* key = const_cast<uint8_t*>(test_case.key.data());
-  uint8_t* digest = const_cast<uint8_t*>(test_case.digest.data());
+  {
+    bytes got_digest(test.out_len);
 
-  bool test = test_blake2b(&Hacl_Blake2b_32_blake2b,
-                           test_case.input.size(),
-                           input,
-                           test_case.key.size(),
-                           key,
-                           test_case.out_len,
-                           digest);
-  EXPECT_TRUE(test);
+    Hacl_Blake2b_32_blake2b(test.out_len,
+                            got_digest.data(),
+                            test.input.size(),
+                            test.input.data(),
+                            test.key.size(),
+                            test.key.data());
 
-  if (test_case.key.size() == 0) {
-    bytes digest_evercrypt(test_case.out_len, 0);
-    uint8_t* input = const_cast<uint8_t*>(test_case.input.data());
-    EverCrypt_Hash_hash(EverCrypt_Hash_Blake2B_s,
-                        digest_evercrypt.data(),
-                        input,
-                        test_case.input.size());
-    EXPECT_EQ(digest_evercrypt, test_case.digest);
+    bool outcome =
+      compare_and_print(test.out_len, got_digest.data(), test.digest.data());
+
+    EXPECT_TRUE(outcome);
+  }
+
+  // EverCrypt
+  {
+    EverCrypt_AutoConfig2_init();
+
+    if (test.key.size() == 0) {
+      bytes got_digest(test.out_len, 0);
+
+      EverCrypt_Hash_hash(EverCrypt_Hash_Blake2B_s,
+                          got_digest.data(),
+                          test.input.data(),
+                          test.input.size());
+
+      EXPECT_EQ(got_digest, test.digest);
+    }
   }
 }
 
-INSTANTIATE_TEST_SUITE_P(Kat, Blake2bKAT, ::testing::ValuesIn(read_json()));
-INSTANTIATE_TEST_SUITE_P(OfficialKat,
-                         Blake2bKAT,
-                         ::testing::ValuesIn(read_official_json()));
+class Blake2bStreaming : public ::testing::TestWithParam<TestCase>
+{};
+
+TEST_P(Blake2bStreaming, KAT)
+{
+  auto test_case = GetParam();
+
+  {
+    // Skip tests with key.
+    if (test_case.key.size() != 0) {
+      return;
+    }
+
+    bytes got_digest(64);
+
+    // Init
+    Hacl_Streaming_Blake2_blake2b_32_state_s* state =
+      Hacl_Streaming_Blake2_blake2b_32_no_key_create_in();
+    Hacl_Streaming_Blake2_blake2b_32_no_key_init(state);
+
+    // Update
+    Hacl_Streaming_Blake2_blake2b_32_no_key_update(
+      state, test_case.input.data(), test_case.input.size());
+
+    // Finish
+    Hacl_Streaming_Blake2_blake2b_32_no_key_finish(state, got_digest.data());
+    Hacl_Streaming_Blake2_blake2b_32_no_key_free(state);
+
+    EXPECT_EQ(test_case.digest, got_digest);
+  }
+
+#ifdef HACL_CAN_COMPILE_VEC256
+  {
+    hacl_init_cpu_features();
+
+    if (hacl_vec256_support()) {
+      // TODO: Enable this. See https://github.com/project-everest/hacl-star/issues/586
+      //
+      //    bytes got_hash(64);
+      //
+      //    // Init
+      //    Hacl_Streaming_Blake2b_256_blake2b_256_state* state =
+      //      Hacl_Streaming_Blake2b_256_blake2b_256_no_key_create_in();
+      //    Hacl_Streaming_Blake2b_256_blake2b_256_no_key_init(state);
+      //
+      //    // Update
+      //    Hacl_Streaming_Blake2b_256_blake2b_256_no_key_update(
+      //      state, test_case.input.data(), test_case.input.size());
+      //
+      //    // Finish
+      //    Hacl_Streaming_Blake2b_256_blake2b_256_no_key_finish(state,
+      //                                                         got_hash.data());
+      //    Hacl_Streaming_Blake2b_256_blake2b_256_no_key_free(state);
+      //
+      //    EXPECT_EQ(test_case.digest, got_hash);
+    } else {
+      printf(" ! Vec256 was compiled but AVX2 is not available on this CPU.\n");
+    }
+  }
+#endif
+}
+
+INSTANTIATE_TEST_SUITE_P(
+  Kat,
+  Blake2b,
+  ::testing::ValuesIn(read_blake2b_json("blake2b.json")));
+
+INSTANTIATE_TEST_SUITE_P(
+  Official,
+  Blake2b,
+  ::testing::ValuesIn(read_official_json("official.json")));
+
+INSTANTIATE_TEST_SUITE_P(
+  Vectors,
+  Blake2b,
+  ::testing::ValuesIn(read_official_json("vectors2b.json")));
+
+INSTANTIATE_TEST_SUITE_P(
+  Kat,
+  Blake2bStreaming,
+  ::testing::ValuesIn(read_blake2b_json("blake2b.json")));
+
+INSTANTIATE_TEST_SUITE_P(
+  Official,
+  Blake2bStreaming,
+  ::testing::ValuesIn(read_official_json("official.json")));
+
+INSTANTIATE_TEST_SUITE_P(
+  Vectors,
+  Blake2bStreaming,
+  ::testing::ValuesIn(read_official_json("vectors2b.json")));
