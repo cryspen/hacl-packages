2d53ca6a25a24e65a9d6141d3ee80a21bc4af000
Damian Poddebniak <poddebniak@mailbox.org>
2022-08-18 17:21:55 +0200

    tests(EverCrypt): Add hmac tests.

--- tests/hmac.cc
+++ tests/hmac.cc
@@ -6,20 +6,16 @@
  *    - http://opensource.org/licenses/MIT
  */
 #include <fstream>
-
 #include <gtest/gtest.h>
 #include <nlohmann/json.hpp>
 
-#include "hacl-cpu-features.h"
-
 #include "EverCrypt_HMAC.h"
 #include "Hacl_HMAC.h"
-
+#include "evercrypt.h"
+#include "hacl-cpu-features.h"
 #include "util.h"
 
 using json = nlohmann::json;
-
-#define bytes std::vector<uint8_t>
 
 typedef struct
 {
@@ -33,11 +29,9 @@ typedef struct
 } TestCase;
 
 std::vector<TestCase>
-read_json(char* test_dir, size_t full_size)
+read_json(string path, size_t full_size)
 {
-
-  // Read JSON test vector
-  std::ifstream json_test_file(test_dir);
+  std::ifstream json_test_file(path);
   json test_vectors;
   json_test_file >> test_vectors;
 
@@ -66,7 +60,7 @@ read_json(char* test_dir, size_t full_size)
 }
 
 std::vector<TestCase>
-read_cavp_json(char* test_dir)
+read_cavp_json(string test_dir)
 {
   std::ifstream file(test_dir);
   json tests_raw;
@@ -144,32 +138,156 @@ TEST_P(HmacKAT, TryKAT)
   } else {
     EXPECT_NE(tag, test_case.tag);
   }
+}
 
-  // TODO: Evercrypt
+// ----- EverCrypt -------------------------------------------------------------
+
+typedef EverCryptSuite<TestCase> HMACEverCryptSuite;
+
+TEST_P(HMACEverCryptSuite, KAT)
+{
+  EverCryptConfig config;
+  TestCase test;
+  tie(config, test) = this->GetParam();
+
+  EXPECT_EQ(test.key_size >> 3, test.key.size());
+  EXPECT_EQ(test.tag_size >> 3, test.tag.size());
+
+  bytes got_tag(test.full_size);
+  if (test.full_size == 20) {
+    ASSERT_TRUE(EverCrypt_HMAC_is_supported_alg(Spec_Hash_Definitions_SHA1));
+    EverCrypt_HMAC_compute(Spec_Hash_Definitions_SHA1,
+                           got_tag.data(),
+                           test.key.data(),
+                           test.key.size(),
+                           test.msg.data(),
+                           test.msg.size());
+  } else if (test.full_size == 28) {
+    std::cout << "Skipping \"full_size=" << test.full_size << "\"" << std::endl;
+    return;
+  } else if (test.full_size == 32) {
+    ASSERT_TRUE(
+      EverCrypt_HMAC_is_supported_alg(Spec_Hash_Definitions_SHA2_256));
+    EverCrypt_HMAC_compute(Spec_Hash_Definitions_SHA2_256,
+                           got_tag.data(),
+                           test.key.data(),
+                           test.key.size(),
+                           test.msg.data(),
+                           test.msg.size());
+  } else if (test.full_size == 48) {
+    ASSERT_TRUE(
+      EverCrypt_HMAC_is_supported_alg(Spec_Hash_Definitions_SHA2_384));
+    EverCrypt_HMAC_compute(Spec_Hash_Definitions_SHA2_384,
+                           got_tag.data(),
+                           test.key.data(),
+                           test.key.size(),
+                           test.msg.data(),
+                           test.msg.size());
+  } else if (test.full_size == 64) {
+    ASSERT_TRUE(
+      EverCrypt_HMAC_is_supported_alg(Spec_Hash_Definitions_SHA2_512));
+    EverCrypt_HMAC_compute(Spec_Hash_Definitions_SHA2_512,
+                           got_tag.data(),
+                           test.key.data(),
+                           test.key.size(),
+                           test.msg.data(),
+                           test.msg.size());
+  } else {
+    FAIL() << "Unsupported \"full_size\" (" << test.full_size << ")";
+  }
+
+  // XXX: Manually truncate the got_tag ...
+  got_tag.resize(test.tag.size());
+
+  if (test.valid) {
+    EXPECT_EQ(got_tag, test.tag) << bytes_to_hex(got_tag) << std::endl
+                                 << bytes_to_hex(test.tag) << std::endl;
+  } else {
+    EXPECT_NE(got_tag, test.tag);
+  }
+}
+
+// -----------------------------------------------------------------------------
+
+INSTANTIATE_TEST_SUITE_P(HmacSha1Kat,
+                         HmacKAT,
+                         ::testing::ValuesIn(read_json("hmac_sha1_test.json",
+                                                       20)));
+
+INSTANTIATE_TEST_SUITE_P(HmacSha256Kat,
+                         HmacKAT,
+                         ::testing::ValuesIn(read_json("hmac_sha256_test.json",
+                                                       32)));
+INSTANTIATE_TEST_SUITE_P(HmacSha384Kat,
+                         HmacKAT,
+                         ::testing::ValuesIn(read_json("hmac_sha384_test.json",
+                                                       48)));
+INSTANTIATE_TEST_SUITE_P(HmacSha512Kat,
+                         HmacKAT,
+                         ::testing::ValuesIn(read_json("hmac_sha512_test.json",
+                                                       64)));
+
+INSTANTIATE_TEST_SUITE_P(CAVP,
+                         HmacKAT,
+                         ::testing::ValuesIn(read_cavp_json("CAVP_HMAC.json")));
+
+// ----- EverCrypt -------------------------------------------------------------
+
+// Portable (depends on hash) --> sha1, sha2 --> shaext
+vector<EverCryptConfig>
+generate_hmac_configs()
+{
+  vector<EverCryptConfig> configs;
+
+  for (uint32_t i = 0; i < 2; ++i) {
+    configs.push_back(EverCryptConfig{
+      .disable_adx = false,
+      .disable_aesni = false,
+      .disable_avx = false,
+      .disable_avx2 = false,
+      .disable_avx512 = false,
+      .disable_bmi2 = false,
+      .disable_movbe = false,
+      .disable_pclmulqdq = false,
+      .disable_rdrand = false,
+      .disable_shaext = (i & 1) != 0,
+      .disable_sse = false,
+    });
+  }
+
+  return configs;
 }
 
 INSTANTIATE_TEST_SUITE_P(
   HmacSha1Kat,
-  HmacKAT,
-  ::testing::ValuesIn(read_json(const_cast<char*>("hmac_sha1_test.json"), 20)));
+  HMACEverCryptSuite,
+  ::testing::Combine(::testing::ValuesIn(generate_hmac_configs()),
+                     ::testing::ValuesIn(read_json("hmac_sha1_test.json",
+                                                   20))));
 
 INSTANTIATE_TEST_SUITE_P(
   HmacSha256Kat,
-  HmacKAT,
-  ::testing::ValuesIn(read_json(const_cast<char*>("hmac_sha256_test.json"),
-                                32)));
+  HMACEverCryptSuite,
+  ::testing::Combine(::testing::ValuesIn(generate_hmac_configs()),
+                     ::testing::ValuesIn(read_json("hmac_sha256_test.json",
+                                                   32))));
+
 INSTANTIATE_TEST_SUITE_P(
   HmacSha384Kat,
-  HmacKAT,
-  ::testing::ValuesIn(read_json(const_cast<char*>("hmac_sha384_test.json"),
-                                48)));
+  HMACEverCryptSuite,
+  ::testing::Combine(::testing::ValuesIn(generate_hmac_configs()),
+                     ::testing::ValuesIn(read_json("hmac_sha384_test.json",
+                                                   48))));
+
 INSTANTIATE_TEST_SUITE_P(
   HmacSha512Kat,
-  HmacKAT,
-  ::testing::ValuesIn(read_json(const_cast<char*>("hmac_sha512_test.json"),
-                                64)));
+  HMACEverCryptSuite,
+  ::testing::Combine(::testing::ValuesIn(generate_hmac_configs()),
+                     ::testing::ValuesIn(read_json("hmac_sha512_test.json",
+                                                   64))));
 
 INSTANTIATE_TEST_SUITE_P(
   CAVP,
-  HmacKAT,
-  ::testing::ValuesIn(read_cavp_json(const_cast<char*>("CAVP_HMAC.json"))));
+  HMACEverCryptSuite,
+  ::testing::Combine(::testing::ValuesIn(generate_hmac_configs()),
+                     ::testing::ValuesIn(read_cavp_json("CAVP_HMAC.json"))));
