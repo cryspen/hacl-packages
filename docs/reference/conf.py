a2021fcb88387f4e41b825790f59b4ebfb718b56
Damian Poddebniak <poddebniak@mailbox.org>
2022-10-05 15:31:05 +0200

    docs: Use custom CSS to break long functions.

--- docs/reference/conf.py
+++ docs/reference/conf.py
@@ -119,3 +119,15 @@ breathe_projects = {
 
 # Breathe Configuration
 breathe_default_project = "HACL Packages"
+
+# -- Custom CSS --
+
+# These folders are copied to the documentation's HTML output
+html_static_path = ['_static']
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    'css/custom.css',
+]
+
