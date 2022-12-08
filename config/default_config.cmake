7533ee5cdabe8ac03760a25ed3e384eab30b7533
Franziskus Kiefer <franziskuskiefer@gmail.com>
2022-12-08 14:31:44 +0100

    fix default_config.cmake

--- config/default_config.cmake
+++ config/default_config.cmake
@@ -158,6 +158,8 @@ set(INCLUDES
 	${PROJECT_SOURCE_DIR}/include/Hacl_Bignum256_32.h
 	${PROJECT_SOURCE_DIR}/include/Hacl_Bignum_Base.h
 	${PROJECT_SOURCE_DIR}/include/lib_intrinsics.h
+	${PROJECT_SOURCE_DIR}/include/Hacl_IntTypes_Intrinsics.h
+	${PROJECT_SOURCE_DIR}/include/Hacl_IntTypes_Intrinsics_128.h
 	${PROJECT_SOURCE_DIR}/include/Hacl_Bignum.h
 	${PROJECT_SOURCE_DIR}/include/internal/Hacl_Bignum.h
 	${PROJECT_SOURCE_DIR}/include/internal/../Hacl_Bignum.h
@@ -302,6 +304,8 @@ set(PUBLIC_INCLUDES
 	${PROJECT_SOURCE_DIR}/include/Hacl_Bignum256_32.h
 	${PROJECT_SOURCE_DIR}/include/Hacl_Bignum_Base.h
 	${PROJECT_SOURCE_DIR}/include/lib_intrinsics.h
+	${PROJECT_SOURCE_DIR}/include/Hacl_IntTypes_Intrinsics.h
+	${PROJECT_SOURCE_DIR}/include/Hacl_IntTypes_Intrinsics_128.h
 	${PROJECT_SOURCE_DIR}/include/Hacl_Bignum.h
 	${PROJECT_SOURCE_DIR}/include/internal/../Hacl_Bignum.h
 	${PROJECT_SOURCE_DIR}/include/Hacl_Bignum256.h
