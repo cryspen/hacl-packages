676c6d60e427053938e8e7721fee1fcaecc9e4fa
Damian Poddebniak <poddebniak@mailbox.org>
2022-12-12 16:51:46 +0100

    docs/tests: Add NaCl examples.

--- tests/util.h
+++ tests/util.h
@@ -138,3 +138,29 @@ print_hex_ln(size_t bytes_len, uint8_t* bytes)
   printf("\n");
 }
 // ANCHOR_END(print_hex_ln)
+
+// Note: This is used to "hide" keys from examples.
+void
+crypto_box_keypair_alice(uint8_t* sk, uint8_t* pk)
+{
+  bytes sk_bytes = from_hex(
+    "2a10d44276462147f34a899ac72c9fabfefa60591bc22d8c44fe4819c0151080");
+  std::copy(sk_bytes.begin(), sk_bytes.end(), sk);
+
+  bytes pk_bytes = from_hex(
+    "ae496ef306c7c0bda37e8e4076238d2fd433004fdf047ccd2cee6be7fb731136");
+  std::copy(pk_bytes.begin(), pk_bytes.end(), pk);
+}
+
+// Note: This is used to "hide" keys from examples.
+void
+crypto_box_keypair_bob(uint8_t* sk, uint8_t* pk)
+{
+  bytes sk_bytes = from_hex(
+    "9ea6ce368aea3c09577f8e57118acb9cdf9e2b562d035c243a582bd01b0795ed");
+  std::copy(sk_bytes.begin(), sk_bytes.end(), sk);
+
+  bytes pk_bytes = from_hex(
+    "2af1a3b5ed0486f895a54fdd2dfbcd0daa1e0982ef33eeb0d693d6d502c5901a");
+  std::copy(pk_bytes.begin(), pk_bytes.end(), pk);
+}
