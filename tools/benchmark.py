993c1ed5b733a5b6753210dad5e07d85505c236f
Paul-Nicolas Madelaine <pnm@pnm.tf>
2022-12-13 08:00:00 +0100

    add --compare to the benchmark subcommand

--- tools/benchmark.py
+++ tools/benchmark.py
@@ -38,11 +38,73 @@ def run_benchmarks(benchmarks, bin_path):
             subprocess.run(benchmark_cmd, check=True)
 
 
+def compare_benchmarks(benchmarks, path_1, path_2):
+    print("Comparing benchmarks ...")
+
+    if not os.path.exists(path_1) or not os.path.exists(path_2):
+        print("! Nothing is built! Please build first. Aborting!")
+        exit(1)
+    if not os.path.exists("build/benchmark-src"):
+        print("! gbenchmark missing! Aborting!")
+        exit(1)
+
+    compare = "./build/benchmark-src/tools/compare.py"
+
+    for algorithm in benchmarks:
+        for benchmark in benchmarks[algorithm]:
+
+            file_name = Path(benchmark).stem
+            file_name_1 = os.path.join(path_1, file_name + "_benchmark")
+            file_name_2 = os.path.join(path_2, file_name + "_benchmark")
+            if sys.platform == "win32":
+                file_name_1 += ".exe"
+                file_name_2 += ".exe"
+            if not os.path.exists(file_name_1):
+                print("! Benchmark '%s' doesn't exist. Aborting!" % (file_name_1))
+                print("   Running this benchmark requires a build first.")
+                print("   See mach build --help")
+                exit(1)
+            if not os.path.exists(file_name_2):
+                print("! Benchmark '%s' doesn't exist. Aborting!" % (file_name_2))
+                print("   Running this benchmark requires a build first.")
+                print("   See mach build --help")
+                exit(1)
+            out_path = os.path.join(path_1, file_name + "_benchmark.json")
+            benchmark_cmd = [
+                compare,
+                "-d",
+                os.path.join(".", out_path),
+                "benchmarks",
+                os.path.join(".", file_name_1),
+                file_name_2,
+            ]
+            print(" ".join(benchmark_cmd))
+            subprocess.run(benchmark_cmd)
+
+    threshold = 0.2
+    ret = 0
+    for algorithm in benchmarks:
+        for benchmark in benchmarks[algorithm]:
+            file_name = Path(benchmark).stem
+            out_path = os.path.join(path_1, file_name + "_benchmark.json")
+            f = open(out_path)
+            data = json.load(f)
+            # For each benchmark, the result is the overall CPU-time evolution
+            result = data[-1]["measurements"][0]["cpu"]
+            print("{:20} {:+0.2f}".format(file_name, result))
+            # If one result is greater than the threshold, the command fails
+            if result > threshold:
+                ret = 1
+
+    exit(ret)
+
+
 @subcommand(
     [
         argument(
             "-v", "--verbose", help="Make benchmarks verbose.", action="store_true"
         ),
+        argument("--compare", help="Compare against another revision.", type=str),
     ]
 )
 def benchmark(args):
@@ -55,4 +117,11 @@ def benchmark(args):
     # parse file
     config = json.loads(data)
 
-    run_benchmarks(config["benchmarks"], "Release")
+    if args.compare:
+        compare_benchmarks(
+            config["benchmarks"],
+            binary_path("Release"),
+            os.path.join(args.compare, binary_path("Release")),
+        )
+    else:
+        run_benchmarks(config["benchmarks"], "Release")
