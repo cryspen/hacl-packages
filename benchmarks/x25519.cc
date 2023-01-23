272058ee7e7042ac7bc59b2695de97177bab1c3d
Damian Poddebniak <poddebniak@mailbox.org>
2023-01-23 22:21:45 +0100

    chore: Cleanup X25519 benchmarks.

--- benchmarks/x25519.cc
+++ benchmarks/x25519.cc
@@ -6,14 +6,14 @@
  *    - http://opensource.org/licenses/MIT
  */
 
-#include "util.h"
-
 #include "EverCrypt_Curve25519.h"
 #include "Hacl_Curve25519_51.h"
 
+#include "util.h"
+
 #if HACL_CAN_COMPILE_VALE
 #include "Hacl_Curve25519_64.h"
-#endif // HACL_CAN_COMPILE_VALE
+#endif
 
 static void
 setup(bytes& x, bytes& y, bytes& pk_x, bytes& expected_res)
@@ -28,8 +28,10 @@ setup(bytes& x, bytes& y, bytes& pk_x, bytes& expected_res)
     "b2960014ef49f8a2600826857eeb7d6533eec9b40d49c88f160f6f64398c8a47");
 }
 
+// -----------------------------------------------------------------------------
+
 static void
-x25519_51(benchmark::State& state)
+HACL_x25519_51(benchmark::State& state)
 {
   bytes x, y, pk_x, expected_res;
   setup(x, y, pk_x, expected_res);
@@ -46,61 +48,11 @@ x25519_51(benchmark::State& state)
   }
 }
 
-static void
-x25519_51_base(benchmark::State& state)
-{
-  bytes x, y, pk_x, expected_res;
-  setup(x, y, pk_x, expected_res);
-
-  bytes res(32);
-  while (state.KeepRunning()) {
-    Hacl_Curve25519_51_secret_to_public(res.data(), x.data());
-    if (res != pk_x) {
-      state.SkipWithError("Error in x25519 secret to public");
-      break;
-    }
-  }
-}
-
-static void
-x25519_Evercrypt(benchmark::State& state)
-{
-  EverCrypt_AutoConfig2_init();
-
-  bytes x, y, pk_x, expected_res;
-  setup(x, y, pk_x, expected_res);
-  bytes pk(32);
-  EverCrypt_Curve25519_secret_to_public(pk.data(), x.data());
-
-  bytes res(32);
-  while (state.KeepRunning()) {
-    EverCrypt_Curve25519_ecdh(res.data(), y.data(), pk.data());
-    if (res != expected_res) {
-      state.SkipWithError("Error in x25519");
-      break;
-    }
-  }
-}
-
-static void
-x25519_Evercrypt_base(benchmark::State& state)
-{
-  bytes x, y, pk_x, expected_res;
-  setup(x, y, pk_x, expected_res);
-
-  bytes res(32);
-  while (state.KeepRunning()) {
-    EverCrypt_Curve25519_secret_to_public(res.data(), x.data());
-    if (res != pk_x) {
-      state.SkipWithError("Error in x25519 secret to public");
-      break;
-    }
-  }
-}
+BENCHMARK(HACL_x25519_51);
 
 #if HACL_CAN_COMPILE_VALE
 static void
-x25519_64(benchmark::State& state)
+HACL_x25519_64(benchmark::State& state)
 {
   cpu_init();
   if (!vale_x25519_support()) {
@@ -123,32 +75,34 @@ x25519_64(benchmark::State& state)
   }
 }
 
+BENCHMARK(HACL_x25519_64);
+#endif
+
 static void
-x25519_64_base(benchmark::State& state)
+EverCrypt_x25519(benchmark::State& state)
 {
-  cpu_init();
-  if (!vale_x25519_support()) {
-    state.SkipWithError("No vec256 support");
-    return;
-  }
+  EverCrypt_AutoConfig2_init();
 
   bytes x, y, pk_x, expected_res;
   setup(x, y, pk_x, expected_res);
+  bytes pk(32);
+  EverCrypt_Curve25519_secret_to_public(pk.data(), x.data());
 
   bytes res(32);
   while (state.KeepRunning()) {
-    Hacl_Curve25519_64_secret_to_public(res.data(), x.data());
-    if (res != pk_x) {
-      state.SkipWithError("Error in x25519 secret to public");
+    EverCrypt_Curve25519_ecdh(res.data(), y.data(), pk.data());
+    if (res != expected_res) {
+      state.SkipWithError("Error in x25519");
       break;
     }
   }
 }
-#endif // HACL_CAN_COMPILE_VALE
+
+BENCHMARK(EverCrypt_x25519);
 
 #ifndef NO_OPENSSL
 static void
-Openssl_x25519(benchmark::State& state)
+OpenSSL_x25519(benchmark::State& state)
 {
   EVP_PKEY *pkey_a = NULL, *pkey_b = NULL;
   EVP_PKEY_CTX* pctx_a = EVP_PKEY_CTX_new_id(NID_X25519, NULL);
@@ -185,21 +139,83 @@ Openssl_x25519(benchmark::State& state)
   EVP_PKEY_CTX_free(pctx_a);
   EVP_PKEY_CTX_free(pctx_b);
 }
+
+BENCHMARK(OpenSSL_x25519);
 #endif
 
-BENCHMARK(x25519_51);
+// -----------------------------------------------------------------------------
+
+static void
+HACL_x25519_51_base(benchmark::State& state)
+{
+  bytes x, y, pk_x, expected_res;
+  setup(x, y, pk_x, expected_res);
+
+  bytes res(32);
+  while (state.KeepRunning()) {
+    Hacl_Curve25519_51_secret_to_public(res.data(), x.data());
+    if (res != pk_x) {
+      state.SkipWithError("Error in x25519 secret to public");
+      break;
+    }
+  }
+}
+
+BENCHMARK(HACL_x25519_51_base);
+
 #if HACL_CAN_COMPILE_VALE
-BENCHMARK(x25519_64);
-#endif // HACL_CAN_COMPILE_VALE
-BENCHMARK(x25519_Evercrypt);
+static void
+HACL_x25519_64_base(benchmark::State& state)
+{
+  cpu_init();
+  if (!vale_x25519_support()) {
+    state.SkipWithError("No vec256 support");
+    return;
+  }
+
+  bytes x, y, pk_x, expected_res;
+  setup(x, y, pk_x, expected_res);
+
+  bytes res(32);
+  while (state.KeepRunning()) {
+    Hacl_Curve25519_64_secret_to_public(res.data(), x.data());
+    if (res != pk_x) {
+      state.SkipWithError("Error in x25519 secret to public");
+      break;
+    }
+  }
+}
+
+BENCHMARK(HACL_x25519_64_base);
+#endif
+
+static void
+EverCrypt_x25519_base(benchmark::State& state)
+{
+  bytes x, y, pk_x, expected_res;
+  setup(x, y, pk_x, expected_res);
+
+  bytes res(32);
+  while (state.KeepRunning()) {
+    EverCrypt_Curve25519_secret_to_public(res.data(), x.data());
+    if (res != pk_x) {
+      state.SkipWithError("Error in x25519 secret to public");
+      break;
+    }
+  }
+}
+
+BENCHMARK(EverCrypt_x25519_base);
+
 #ifndef NO_OPENSSL
-BENCHMARK(Openssl_x25519);
-#endif
+static void
+OpenSSL_x25519_base(benchmark::State& state)
+{
+  // TODO
+  state.SkipWithError("Unimplemented");
+}
 
-BENCHMARK(x25519_51_base);
-#if HACL_CAN_COMPILE_VALE
-BENCHMARK(x25519_64_base);
-#endif // HACL_CAN_COMPILE_VALE
-BENCHMARK(x25519_Evercrypt_base);
+BENCHMARK(OpenSSL_x25519_base);
+#endif
 
 BENCHMARK_MAIN();
