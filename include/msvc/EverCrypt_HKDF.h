d1f02db24eac9599b10e71aa7470fd4567c8c8bc
Franziskus Kiefer <franziskuskiefer@gmail.com>
2023-01-23 10:52:41 +0100

    update to hacl-star 17d4b3d0fb3f8a56af4e5463d88c78c25a8d4571

--- include/msvc/EverCrypt_HKDF.h
+++ include/msvc/EverCrypt_HKDF.h
@@ -1,6 +1,7 @@
 /* MIT License
  *
- * Copyright (c) 2016-2020 INRIA, CMU and Microsoft Corporation
+ * Copyright (c) 2016-2022 INRIA, CMU and Microsoft Corporation
+ * Copyright (c) 2022-2023 HACL* Contributors
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -36,121 +37,6 @@ extern "C" {
 
 
 #include "Hacl_Spec.h"
-#include "EverCrypt_HMAC.h"
-
-void
-EverCrypt_HKDF_expand_sha1(
-  uint8_t *okm,
-  uint8_t *prk,
-  uint32_t prklen,
-  uint8_t *info,
-  uint32_t infolen,
-  uint32_t len
-);
-
-void
-EverCrypt_HKDF_extract_sha1(
-  uint8_t *prk,
-  uint8_t *salt,
-  uint32_t saltlen,
-  uint8_t *ikm,
-  uint32_t ikmlen
-);
-
-void
-EverCrypt_HKDF_expand_sha2_256(
-  uint8_t *okm,
-  uint8_t *prk,
-  uint32_t prklen,
-  uint8_t *info,
-  uint32_t infolen,
-  uint32_t len
-);
-
-void
-EverCrypt_HKDF_extract_sha2_256(
-  uint8_t *prk,
-  uint8_t *salt,
-  uint32_t saltlen,
-  uint8_t *ikm,
-  uint32_t ikmlen
-);
-
-void
-EverCrypt_HKDF_expand_sha2_384(
-  uint8_t *okm,
-  uint8_t *prk,
-  uint32_t prklen,
-  uint8_t *info,
-  uint32_t infolen,
-  uint32_t len
-);
-
-void
-EverCrypt_HKDF_extract_sha2_384(
-  uint8_t *prk,
-  uint8_t *salt,
-  uint32_t saltlen,
-  uint8_t *ikm,
-  uint32_t ikmlen
-);
-
-void
-EverCrypt_HKDF_expand_sha2_512(
-  uint8_t *okm,
-  uint8_t *prk,
-  uint32_t prklen,
-  uint8_t *info,
-  uint32_t infolen,
-  uint32_t len
-);
-
-void
-EverCrypt_HKDF_extract_sha2_512(
-  uint8_t *prk,
-  uint8_t *salt,
-  uint32_t saltlen,
-  uint8_t *ikm,
-  uint32_t ikmlen
-);
-
-void
-EverCrypt_HKDF_expand_blake2s(
-  uint8_t *okm,
-  uint8_t *prk,
-  uint32_t prklen,
-  uint8_t *info,
-  uint32_t infolen,
-  uint32_t len
-);
-
-void
-EverCrypt_HKDF_extract_blake2s(
-  uint8_t *prk,
-  uint8_t *salt,
-  uint32_t saltlen,
-  uint8_t *ikm,
-  uint32_t ikmlen
-);
-
-void
-EverCrypt_HKDF_expand_blake2b(
-  uint8_t *okm,
-  uint8_t *prk,
-  uint32_t prklen,
-  uint8_t *info,
-  uint32_t infolen,
-  uint32_t len
-);
-
-void
-EverCrypt_HKDF_extract_blake2b(
-  uint8_t *prk,
-  uint8_t *salt,
-  uint32_t saltlen,
-  uint8_t *ikm,
-  uint32_t ikmlen
-);
 
 /**
 Expand pseudorandom key to desired length.
