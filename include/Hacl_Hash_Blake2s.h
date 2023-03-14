28253e105fa9b56cf716129d63e2b425f494a99c
Paul-Nicolas Madelaine <pnm@pnm.tf>
2023-03-14 18:17:19 +0100

    update to HACL* 72627c16662a39b57049a9b43d429f0429fac593

--- include/Hacl_Hash_Blake2s.h
+++ include/Hacl_Hash_Blake2s.h
@@ -23,8 +23,8 @@
  */
 
 
-#ifndef __Hacl_Hash_Blake2b_32_H
-#define __Hacl_Hash_Blake2b_32_H
+#ifndef __Hacl_Hash_Blake2s_H
+#define __Hacl_Hash_Blake2s_H
 
 #if defined(__cplusplus)
 extern "C" {
@@ -36,53 +36,52 @@ extern "C" {
 #include "krml/internal/target.h"
 
 #include "Lib_Memzero0.h"
-#include "Hacl_Krmllib.h"
 
-void Hacl_Hash_Blake2b_32_init(uint64_t *hash, uint32_t kk, uint32_t nn);
+void Hacl_Hash_Blake2s_init(uint32_t *hash, uint32_t kk, uint32_t nn);
 
 void
-Hacl_Hash_Blake2b_32_update_key(
-  uint64_t *wv,
-  uint64_t *hash,
+Hacl_Hash_Blake2s_update_key(
+  uint32_t *wv,
+  uint32_t *hash,
   uint32_t kk,
   uint8_t *k,
   uint32_t ll
 );
 
 void
-Hacl_Hash_Blake2b_32_update_multi(
+Hacl_Hash_Blake2s_update_multi(
   uint32_t len,
-  uint64_t *wv,
-  uint64_t *hash,
-  FStar_UInt128_uint128 prev,
+  uint32_t *wv,
+  uint32_t *hash,
+  uint64_t prev,
   uint8_t *blocks,
   uint32_t nb
 );
 
 void
-Hacl_Hash_Blake2b_32_update_last(
+Hacl_Hash_Blake2s_update_last(
   uint32_t len,
-  uint64_t *wv,
-  uint64_t *hash,
-  FStar_UInt128_uint128 prev,
+  uint32_t *wv,
+  uint32_t *hash,
+  uint64_t prev,
   uint32_t rem,
   uint8_t *d
 );
 
-void Hacl_Hash_Blake2b_32_finish(uint32_t nn, uint8_t *output, uint64_t *hash);
+void Hacl_Hash_Blake2s_finish(uint32_t nn, uint8_t *output, uint32_t *hash);
 
 /**
-Write the BLAKE2b digest of message `input` using key `key` into `output`.
+Write the BLAKE2s digest of message `input` using key `key` into `output`.
 
 @param output Pointer to `output_len` bytes of memory where the digest is written to.
-@param output_len Length of the to-be-generated digest with 1 <= `output_len` <= 64.
+@param output_len Length of the to-be-generated digest with 1 <= `output_len` <= 32.
 @param input Pointer to `input_len` bytes of memory where the input message is read from.
 @param input_len Length of the input message.
 @param key Pointer to `key_len` bytes of memory where the key is read from.
 @param key_len Length of the key. Can be 0.
 */
 void
-Hacl_Hash_Blake2b_32_hash_with_key(
+Hacl_Hash_Blake2s_hash_with_key(
   uint8_t *output,
   uint32_t output_len,
   uint8_t *input,
@@ -91,56 +90,52 @@ Hacl_Hash_Blake2b_32_hash_with_key(
   uint32_t key_len
 );
 
-uint64_t *Hacl_Hash_Blake2b_32_malloc_with_key(void);
+uint32_t *Hacl_Hash_Blake2s_malloc_with_key(void);
 
-typedef struct Hacl_Hash_Blake2b_32_block_state_t_s
+typedef struct Hacl_Hash_Blake2s_block_state_t_s
 {
-  uint64_t *fst;
-  uint64_t *snd;
+  uint32_t *fst;
+  uint32_t *snd;
 }
-Hacl_Hash_Blake2b_32_block_state_t;
+Hacl_Hash_Blake2s_block_state_t;
 
-typedef struct Hacl_Hash_Blake2b_32_state_t_s
+typedef struct Hacl_Hash_Blake2s_state_t_s
 {
-  Hacl_Hash_Blake2b_32_block_state_t block_state;
+  Hacl_Hash_Blake2s_block_state_t block_state;
   uint8_t *buf;
   uint64_t total_len;
 }
-Hacl_Hash_Blake2b_32_state_t;
+Hacl_Hash_Blake2s_state_t;
 
 /**
   State allocation function when there is no key
 */
-Hacl_Hash_Blake2b_32_state_t *Hacl_Hash_Blake2b_32_malloc(void);
+Hacl_Hash_Blake2s_state_t *Hacl_Hash_Blake2s_malloc(void);
 
 /**
   Re-initialization function when there is no key
 */
-void Hacl_Hash_Blake2b_32_reset(Hacl_Hash_Blake2b_32_state_t *state);
+void Hacl_Hash_Blake2s_reset(Hacl_Hash_Blake2s_state_t *state);
 
 /**
   Update function when there is no key; 0 = success, 1 = max length exceeded
 */
 uint32_t
-Hacl_Hash_Blake2b_32_update(
-  Hacl_Hash_Blake2b_32_state_t *state,
-  uint8_t *chunk,
-  uint32_t chunk_len
-);
+Hacl_Hash_Blake2s_update(Hacl_Hash_Blake2s_state_t *state, uint8_t *chunk, uint32_t chunk_len);
 
 /**
   Finish function when there is no key
 */
-void Hacl_Hash_Blake2b_32_digest(Hacl_Hash_Blake2b_32_state_t *state, uint8_t *output);
+void Hacl_Hash_Blake2s_digest(Hacl_Hash_Blake2s_state_t *state, uint8_t *output);
 
 /**
   Free state function when there is no key
 */
-void Hacl_Hash_Blake2b_32_free(Hacl_Hash_Blake2b_32_state_t *state);
+void Hacl_Hash_Blake2s_free(Hacl_Hash_Blake2s_state_t *state);
 
 #if defined(__cplusplus)
 }
 #endif
 
-#define __Hacl_Hash_Blake2b_32_H_DEFINED
+#define __Hacl_Hash_Blake2s_H_DEFINED
 #endif
--- ocaml/lib/Hacl_Hash_SHA1_bindings.ml
+++ ocaml/lib/Hacl_Hash_SHA1_bindings.ml
@@ -5,25 +5,24 @@ module Bindings(F:Cstubs.FOREIGN) =
     module Hacl_Streaming_Types_applied =
       (Hacl_Streaming_Types_bindings.Bindings)(Hacl_Streaming_Types_stubs)
     open Hacl_Streaming_Types_applied
-    let hacl_Hash_SHA1_legacy_init =
-      foreign "Hacl_Hash_SHA1_legacy_init"
-        ((ptr uint32_t) @-> (returning void))
-    let hacl_Hash_SHA1_legacy_finish =
-      foreign "Hacl_Hash_SHA1_legacy_finish"
+    let hacl_Hash_SHA1_init =
+      foreign "Hacl_Hash_SHA1_init" ((ptr uint32_t) @-> (returning void))
+    let hacl_Hash_SHA1_finish =
+      foreign "Hacl_Hash_SHA1_finish"
         ((ptr uint32_t) @-> (ocaml_bytes @-> (returning void)))
-    let hacl_Hash_SHA1_legacy_update_multi =
-      foreign "Hacl_Hash_SHA1_legacy_update_multi"
+    let hacl_Hash_SHA1_update_multi =
+      foreign "Hacl_Hash_SHA1_update_multi"
         ((ptr uint32_t) @-> (ocaml_bytes @-> (uint32_t @-> (returning void))))
-    let hacl_Hash_SHA1_legacy_update_last =
-      foreign "Hacl_Hash_SHA1_legacy_update_last"
+    let hacl_Hash_SHA1_update_last =
+      foreign "Hacl_Hash_SHA1_update_last"
         ((ptr uint32_t) @->
            (uint64_t @-> (ocaml_bytes @-> (uint32_t @-> (returning void)))))
-    let hacl_Hash_SHA1_legacy_hash =
-      foreign "Hacl_Hash_SHA1_legacy_hash"
+    let hacl_Hash_SHA1_hash_oneshot =
+      foreign "Hacl_Hash_SHA1_hash_oneshot"
         (ocaml_bytes @-> (ocaml_bytes @-> (uint32_t @-> (returning void))))
-    type hacl_Hash_SHA1_state = hacl_Streaming_MD_state_32
-    let hacl_Hash_SHA1_state =
-      typedef hacl_Streaming_MD_state_32 "Hacl_Hash_SHA1_state"
+    type hacl_Hash_SHA1_state_t = hacl_Streaming_MD_state_32
+    let hacl_Hash_SHA1_state_t =
+      typedef hacl_Streaming_MD_state_32 "Hacl_Hash_SHA1_state_t"
     let hacl_Hash_SHA1_malloc =
       foreign "Hacl_Hash_SHA1_malloc"
         (void @-> (returning (ptr hacl_Streaming_MD_state_32)))
