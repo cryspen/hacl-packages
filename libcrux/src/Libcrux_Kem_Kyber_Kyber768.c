11b95a7677e92bedc27455e993072184ef67d7fc
Franziskus Kiefer <franziskuskiefer@gmail.com>
2023-12-11 11:43:21 +0100

    wip

--- libcrux/src/Libcrux_Kem_Kyber_Kyber768.c
+++ libcrux/src/Libcrux_Kem_Kyber_Kyber768.c
@@ -1,13 +1,14 @@
 #include <string.h>
 
 #include "Libcrux_Kem_Kyber_Kyber768.h"
+#include "libcrux_kyber.h"
 
 int
 Libcrux_Kyber768_GenerateKeyPair(uint8_t* pk,
                                  uint8_t* sk,
                                  const uint8_t* randomness)
 {
-  (void)randomness;
+  libcrux_kyber_kyber768_generate_key_pair_768(randomness)
   memset(pk, 0, KYBER768_PUBLICKEYBYTES);
   memset(sk, 0, KYBER768_SECRETKEYBYTES);
   return 0;
