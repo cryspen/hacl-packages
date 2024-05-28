cb8a08c3c1b36f5ae1218bb7cec54be89a63df8a
Franziskus Kiefer <franziskuskiefer@gmail.com>
2024-05-28 21:21:40 +0200

    sha3 from libcrux with neon

--- libcrux/include/eurydice_glue.h
+++ libcrux/include/eurydice_glue.h
@@ -52,15 +52,10 @@ typedef struct {
 #define Eurydice_array_eq(sz, a1, a2, t, _, _ret_t) (memcmp(a1, a2, sz * sizeof(t)) == 0)
 #define core_array_equality___core__cmp__PartialEq__Array_B__N___for__Array_A__N____eq Eurydice_array_eq
 
-#define core_convert___core__convert__AsMut__Slice_T___for__Slice_T___9__as_mut(subslice, _t, _ret_t) subslice
-// FIXME: This isn't great
-#define core_convert___core__convert__Into_U__for_T__3__into(src, srd_t, ret_t, _ret_t) core_convert___core__convert__Into_##ret_t(src)
-
 #define core_slice___Slice_T___split_at(slice, mid, element_type, ret_t) \
   ((ret_t){ \
     .fst = EURYDICE_SLICE((element_type*)slice.ptr, 0, mid), \
     .snd = EURYDICE_SLICE((element_type*)slice.ptr, mid, slice.len)})
-
 #define core_slice___Slice_T___split_at_mut(slice, mid, element_type, ret_t) \
   ((ret_t){ \
     .fst = EURYDICE_SLICE((element_type*)slice.ptr, 0, mid), \
--- libcrux/include/libcrux_hacl_glue.h
+++ libcrux/include/libcrux_hacl_glue.h
@@ -9,31 +9,9 @@ extern "C"
 
 #include "Eurydice.h"
 
+#include "libcrux_digest.h"
 #include <stdint.h>
 #include <string.h>
-#include "libcrux_digest.h"
-
-typedef struct KeccakState1{
-  uint64_t* st;
-} libcrux_sha3_portable_KeccakState1;
-
-libcrux_sha3_portable_KeccakState1
-libcrux_sha3_portable_incremental_shake128_init(void);
-
-void
-libcrux_sha3_portable_incremental_shake128_absorb_final(
-  libcrux_sha3_portable_KeccakState1* state,
-  Eurydice_slice x1);
-
-void
-libcrux_sha3_portable_incremental_shake128_squeeze_next_block(
-  libcrux_sha3_portable_KeccakState1* x0,
-  Eurydice_slice x1);
-
-void
-libcrux_sha3_portable_incremental_shake128_squeeze_first_three_blocks(
-  libcrux_sha3_portable_KeccakState1* x0,
-  Eurydice_slice x1);
 
 #if defined(__cplusplus)
 }
